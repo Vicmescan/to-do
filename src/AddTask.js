1ae7084f4b398ab9a037354d13ef17063143bf6a
vicmescan <mestrecanto@gmail.com>
2022-06-16 10:24:09 +0200

    preparing to post in github

--- src/AddTask.js
+++ src/AddTask.js
@@ -9,9 +9,9 @@ const AddTask = (props) => {
     // add a task to the todo list when the user clicks on the button
     const buttonClick = (e) => {
         if (input.length > 0) {
-        e.preventDefault();
-        props.setTodoList(oldArr => [...oldArr, input])
-        setInput("");
+            e.preventDefault();
+            props.setTodoList(oldArr => [...oldArr, input])
+            setInput("");
         } else {
             e.preventDefault();
             alert("Please enter a task");
@@ -22,10 +22,10 @@ const AddTask = (props) => {
     return (
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control type="text" placeholder="Enter new task" onChange={e => setInput(e.target.value)} value={input} style={{boxShadow: "1px 1px 10px black"}} />
+                <Form.Control type="text" placeholder="Enter new task" onChange={e => setInput(e.target.value)} value={input} style={{ boxShadow: "1px 1px 10px black" }} />
             </Form.Group>
             <div className="mb-2">
-                <Button style={{ boxShadow: "1.5px 1.5px 10px black", textShadow: "1.5px 1.5px 10px black" , marginLeft: "30%" }} variant="secondary" size="lg" onClick={buttonClick} type="submit" >
+                <Button style={{ boxShadow: "1.5px 1.5px 10px black", textShadow: "1.5px 1.5px 10px black", marginLeft: "30%" }} variant="secondary" size="lg" onClick={buttonClick} type="submit" >
                     Add Task
                 </Button>
             </div>
